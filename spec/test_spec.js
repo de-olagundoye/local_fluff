9499c5289763ac24ed1487f5545a1b4f39fd60dc
De Olagundoye <de.olagundoye@gmail.com>
2016-02-24 21:59:14 -0500

    remove karma

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -1,15 +1,3 @@
-describe("An AngularJS test suite", function (){
-  beforeEach(module());
-
-  it('should have tests', function(){
-    expect(true).toBe(true);
-  });
-
-  it('should inject dependencies', inject(function ($resource) {
-    expect($resource).toBeDefined();
-  }))
-});
-
 describe("+", function(){
   it("adds two numbers together", function(){
     expect(2 + 2).toEqual(4);
